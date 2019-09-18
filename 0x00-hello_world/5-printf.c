28c853975b855012785804a14a018530b2112bb6
Nzparra <nzparra@gmail.com>
2019-09-18 18:20:02 +0000

    Prints with function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description : main - Prints a sentence with printf.
+ * Return: 0 if all its ok.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

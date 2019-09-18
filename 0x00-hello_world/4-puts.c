96f7364942a50e0ffac7dd513cfe8f4c49139c91
Nzparra <nzparra@gmail.com>
2019-09-18 17:33:49 +0000

    Add comments of description how the program works

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+\*
+ *Description: This program prints a sentence
+ *Only using the puts function
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

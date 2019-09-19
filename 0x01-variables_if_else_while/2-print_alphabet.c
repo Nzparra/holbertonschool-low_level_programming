ab785a7f8209cceb45b1cc73da16191b0b9a632e
Nzparra <nzparra@gmail.com>
2019-09-19 17:17:13 +0000

    Prints alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Description : main - Prints a number and if is positive or negative
+ * Return: 0 if all its ok.
+ */
+int main(void)
+{
+	char l;
+		for (l = 'a'; l <= 'z'; ++l)
+			putchar(l);
+		putchar('\n');
+		return (0);
+}

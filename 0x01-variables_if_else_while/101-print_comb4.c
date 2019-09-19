2a830ae6057bfb0a7af64bcf5e20ff88761f3ccc
Nzparra <nzparra@gmail.com>
2019-09-19 21:28:48 +0000

    prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * Description : main - Prints number of base 10
+ * Return: 0 if all its ok.
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+	int c = '0';
+
+		while (a <= '9')
+		{
+			while (b <= '9')
+			{
+				while (c <= '9')
+				{
+					if ((a < b && b < c && a != b && b != c))
+					{
+						putchar(a);
+						putchar(b);
+						putchar(c);
+						if (a == '7' && b == '8' && c == '9')
+						{
+							putchar('\n');
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				c++;
+				}
+			c = '0';
+			b++;
+			}
+		b = '0';
+		a++;
+		}
+	return (0);
+}

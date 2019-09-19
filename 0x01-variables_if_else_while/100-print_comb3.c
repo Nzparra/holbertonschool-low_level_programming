0918f296c275fd6dfeaa747fb92a4c40dfa8de09
Nzparra <nzparra@gmail.com>
2019-09-19 20:41:25 +0000

    All possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,29 +8,28 @@ int main(void)
 	int a = '0';
 	int b = '0';
 
-		while (a<= '9')
+		while (a <= '9')
 		{
 			while (b <= '9')
-			{	
-				if((a < b && a != b))
+			{
+				if ((a < b && a != b))
 				{
 					putchar(a);
 					putchar(b);
-					if(a == '8' && b=='9')
+					if (a == '8' && b == '9')
 					{
-					       putchar('\n');
+						putchar('\n');
 					}
-			 		else
+					else
 					{
 					putchar(',');
 					putchar(' ');
 					}
-				}		
+				}
 			b++;
-			}	
+			}
 		b = '0';
 		a++;
 		}
 		return (0);
-		
 }

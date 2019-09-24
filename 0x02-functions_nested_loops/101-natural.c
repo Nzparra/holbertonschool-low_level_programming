9653b42567bddd1af09b0c5eee62519de77b571b
Nzparra <nzparra@gmail.com>
2019-09-24 03:03:51 +0000

    Prints multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * main - Prints the sum of multiples of 3 and 5
+  * Return : 0 if all is ok
+  */
+int main(void)
+{
+	int a;
+	int c;
+
+	c = 0;
+	for (a = 3; a <= 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			c = c + a;
+		}
+	}
+	printf("%d\n", c);
+	return (0);
+}

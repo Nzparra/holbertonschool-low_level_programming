a1db17aa3e99912e5275bfad2f1c2022cbdceeec
Nzparra <nzparra@gmail.com>
2019-09-26 17:28:11 +0000

    Fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,22 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		else
-		{
-		if (c % 3 == 0)
+		else if(c % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else
-		{
-		if (c % 5 == 0)
+		else if (c % 5 == 0)
 		{
 			printf("Buzz");
 		}
 		else
-		{
+		{	
 			printf("%d", c);
 		}
+		if (c != 100)
+		{
+			printf(" ");
 		}
-		}
-		printf(" ");
 	}
 	printf("\n");
 	return (0);

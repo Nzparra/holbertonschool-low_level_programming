578ae1faa3aed73cfc2190a3f1a79c5dcc1ccf6d
Nzparra <nzparra@gmail.com>
2019-10-01 15:20:53 +0000

    Function convert a string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+  * _atoi - Print a number
+  * @s : char to evaluate
+  * Return: Always 0.
+  */
+int _atoi(char *s)
+{
+	int negative;
+	int l;
+	int number;
+
+	number = 0;
+	negative = 1;
+	for (l = 0 ; s[l] != '\0'; l++)
+	{
+		if (s[l] == '-')
+		{
+			negative = negative * -1;
+		}
+		if (s[l] >= '0' && s[l] <= '9')
+		{
+			number = (number * 10) + s[l] - '0';
+		}
+	}
+	return (number * negative);
+}

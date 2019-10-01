b90ad547fe2b1e2c54ab0cd3609d7052fd271e1e
Nzparra <nzparra@gmail.com>
2019-10-01 21:53:52 +0000

    Updated

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - Keygen
+  *
+  * Return: Null.
+  */
+
+int main(void)
+{
+	int num_random;
+	int sum;
+
+	srand(time(0));
+	sum = 0;
+	while (1)
+	{
+		num_random = 32 + rand() % 95;
+		if (sum + num_random == 2772)
+		{
+			putchar(num_random);
+			break;
+		}
+		else if (sum + num_random + 32 < 2772)
+		{
+			putchar(num_random);
+			sum += num_random;
+		}
+	}
+}

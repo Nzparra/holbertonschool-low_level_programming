f14a6b32b6d17aa7f8eb34d3181cbf89d4a83b6d
Nzparra <nzparra@gmail.com>
2019-10-01 14:00:17 +0000

    Fixed styles

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _puts - Print a string
+ * puts2 - Print a string
  * @str : print the string
  * Return: Always 0.
  */
@@ -9,7 +9,7 @@ void puts2(char *str)
 	int i;
 	char b;
 
-	for (i = 0; str[i] != '\0'; i+=2)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		b = str[i];
 		_putchar(b);

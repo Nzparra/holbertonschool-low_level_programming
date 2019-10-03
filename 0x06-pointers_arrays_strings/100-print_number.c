5be1cdc6a1714c4945935c1c139cc2b987a673ee
Nzparra <nzparra@gmail.com>
2019-10-03 19:31:41 +0000

    Print a number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * print_number -Print a number.
+  * @n : Int to print.
+  * Return : null.
+  */
+void print_number(int n)
+{
+	unsigned int b;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	b = n;
+	if (b / 10)
+	{
+		print_number(b / 10);
+	}
+	_putchar(b % 10 + '0');
+}

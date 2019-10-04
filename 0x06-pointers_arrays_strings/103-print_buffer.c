066b64d51a162d64f73affc1c7ad8151e6e14530
Nzparra <nzparra@gmail.com>
2019-10-04 04:30:00 +0000

    Updated body

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -3,8 +3,20 @@
   * print_buffer - Prints a buffer
   * @b: pointer
   * @size: Bytes of the buffer
-  * Return: nothing
+  * Return: null
   */
 void print_buffer(char *b, int size)
 {
+	int loop1;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (loop1 = 0; loop1 < size; loop1 = loop1 + 10)
+		{
+			printf("%08x: ", loop1);
+			printf("\n");
+		}
+	}
 }

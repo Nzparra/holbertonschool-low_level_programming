38f533af3cc161cd6d96aa5b3a9442ecba00a687
Nzparra <nzparra@gmail.com>
2019-10-07 18:14:49 +0000

    Locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+  * match - Function that searches a string
+  * @haystack : string
+  * @needle : character to match
+  * Return: Pointer to the byte s that matches
+  */
+int match(char *haystack, char *needle)
+{
+	while (*haystack && *needle)
+	{
+		if (*haystack != *needle)
+		{
+			return (0);
+		}
+		haystack++;
+		needle++;
+	}
+	return (*needle == '\0');
+}
+/**
+  * _strstr - Function that searches a string
+  * @haystack : string
+  * @needle : character to match
+  * Return: Pointer to the byte s that matches
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if ((*haystack == *needle) && match(haystack, needle))
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
+
+
+

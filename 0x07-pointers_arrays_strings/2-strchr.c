fba195c827a50be780180d4a14ae9b385245ea8b
Nzparra <nzparra@gmail.com>
2019-10-07 17:07:30 +0000

    Fixed style

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,12 +3,12 @@
   * _strchr - Function that locates a character in a string
   * @s : string
   * @c : character to match
-  * Return: Pointer to the 1 occurrence of c in s or NULL if the character is not found
+  * Return: Pointer to the 1 occurrence of c in s or NULL if is not found
   */
 char *_strchr(char *s, char c)
 {
 
-	for (;*s != '\0'; s++)
+	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 		{

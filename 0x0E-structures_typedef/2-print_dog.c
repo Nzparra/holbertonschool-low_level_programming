80c6b6f03eeb543fb97d3864fc8f14e294b73075
Nzparra <nzparra@gmail.com>
2019-10-21 20:10:16 +0000

    [Updated]Nil case

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		d->name != NULL ? printf("Name: %s\n", d->name) : printf("Name: (nil\n)");
+		d->name != NULL ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
 		d->owner != NULL ?
-			printf("Owner: %s\n", d->owner) : printf("Owner: (nil\n)");
+			printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 	}
 }

4aab1a3c2b5b3ba6c27e2ef6514b1750a91351f7
Nzparra <nzparra@gmail.com>
2019-10-23 17:56:05 +0000

    [Fix]NULL CASE

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t c;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
 	for (c = 0; c < size; c++)
 	{
 		action(array[c]);

ad54039711acb997f9df6739888a0a9c0ddc380d
Nzparra <nzparra@gmail.com>
2019-10-31 18:08:58 +0000

    [add]Assembly prints

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int first(void)
+{
+    printf("A tortoise, having pretty good sense of a hare's nature, challenges one to a race.\n");
+    printf("I bore my house upon my back!\n");
+    return (0);
+}
+
+

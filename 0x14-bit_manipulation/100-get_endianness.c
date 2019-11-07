3b145d2ab7972a31a1e21f44e25a25b435a97cb8
Nzparra <nzparra@gmail.com>
2019-11-07 21:32:41 +0000

    [Add]Checks the endiannes

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * get_endianness - Check the endiannes
+ * Return: 0 big endian or 1 little endian
+ */
+int get_endianness(void)
+{
+	unsigned int c;
+
+	return ((int) (((char *)&c)[0]));
+}

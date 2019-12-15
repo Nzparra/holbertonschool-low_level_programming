cd0d588dba251c3056c83e9005423584676405b5
Nzparra <nzparra@gmail.com>
2019-12-15 16:31:34 +0000

    [Add]Free dlist

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a dlist
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp, *h;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	tmp = head;
+	while (tmp->prev)
+	{
+		tmp = tmp->prev;
+	}
+	while (tmp)
+	{
+		h = tmp;
+		tmp = tmp->next;
+		free(h);
+	}
+}

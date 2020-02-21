ea34270d7124728e3bd8bc2261b14caee452eb77
Nicolas <nzparra@gmail.com>
2020-02-21 18:57:27 -0500

    [Fix]Grid loop

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,21 +2,18 @@
 """Calculate perimterer"""
 
 
-def islnad_perimeter(grid):
+def island_perimeter(grid):
     """perimeter"""
-    width = len(grid[0]) - 1
-    height = len(grid) - 1
-    if width == 0 or height == 0:
-        return 0
     per = 0
-    for c in range(width + 1):
-        for d in range(height + 1):
-            if c == width or grid[d][c + 1] == 0:
-                per += 1
-            if c == 0 or grid[d][c - 1] == 0:
-                per += 1
-            if d == height or grid[d + 1][c] == 0:
-                per += 1
-            if d == 0 or grid[d - 1][c] == 0:
-                per += 1
+    for c in range(len(grid)):
+        for d in range(len(grid[c])):
+            if grid[c][d] == 1:
+                if c == len(grid) - 1 or grid[c + 1][d] == 0:
+                    per += 1
+                if c == 0 or grid[c - 1][d] == 0:
+                    per += 1
+                if d == len(grid) - 1 or grid[c][d + 1] == 0:
+                    per += 1
+                if d == 0 or grid[c][d - 1] == 0:
+                    per += 1
     return (per)
